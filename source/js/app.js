c791379cff097bf9fe15d7b485ac36a3d4e16f79
DmitriyVls <n7155ee@gmail.com>
2022-12-12 01:14:11 +0300

    блок с котами, логика слайдера

--- source/js/app.js
+++ source/js/app.js
@@ -1,7 +1,16 @@
 const burger = document.querySelector('.burger__button');
 const menu = document.querySelector('.header__menu ');
+const range = document.querySelector('.slider__range')
+const rangeBeforeImg = document.querySelector ('.example__picture-before');
 
 burger.addEventListener('click', function(event){
     menu.classList.toggle('header__menu--active')
 });
 
+document.addEventListener('DOMContentLoaded', function(){
+    range.addEventListener('input', function(event){
+        rangeBeforeImg.style.width = (event.target.value ) + "%";
+
+
+    },{passive: true})
+})
